982950f2303e2b5b1ca1e6c3a69653766d23c65d
Tushar Nagpal <tushar8.nagpal@gmail.com>
2024-02-15 20:21:49 +0530

    another commit

--- src/components/Pallet.jsx
+++ src/components/Pallet.jsx
@@ -24,7 +24,7 @@ export default function Pallet(){
                     {
                         switch(count) {
   
-                            case 1:   return <h1>Will you be my valentine? {"(Dare to click NO!)"}</h1>;
+                            case 1:   return <h1>Will you be my valentine?(Dare to press No!)</h1>;
                             case 2:   return <h1>You know you cannot say no!(try again)</h1>;
                             case 3:   return <h1>Kabtak no dabaogi?</h1>;
                             case 4:   return <h1>Dabati raho,i have plenty of time</h1>;
@@ -34,7 +34,7 @@ export default function Pallet(){
                             default:      return <h1>Pretty Pleas{e}?</h1>
                           }
                     })()
-                }</div>:<h1>Love You Niharaaaa!!{"(Happy Valentine's)"}</h1>}
+                }</div>:<h1>Love You Jyotuduuuuu!!{"(Happy Valentine's)"}</h1>}
             {yes?<div>
                 <button className="btn" style={yesstyle} onClick={()=>{setYes(false)}}></button>
                 {count<16?<button className="nobutton" style={nostyle} onClick={()=>{setCount(count+1); if(count>5)setE(e+"e")}}></button>:null}
