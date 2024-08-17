3ed82dd0734822a28da6b46c25c7ec21f591c537
jontybot <70278767+jontybot@users.noreply.github.com>
2024-08-18 01:19:08 +0530

    Update Pallet.jsx
    val to sorry

--- src/components/Pallet.jsx
+++ src/components/Pallet.jsx
@@ -24,17 +24,17 @@ export default function Pallet(){
                     {
                         switch(count) {
   
-                            case 1:   return <h1>Will you be my valentine?(Dare to say No!)</h1>;
-                            case 2:   return <h1>You know you cannot say no!(try again)</h1>;
+                            case 1:   return <h1>Ambikaaa, i know you are mad with me, but can you please forgive this nasamajh insaan?????(Pakka dobara nai hoga)</h1>;
+                            case 2:   return <h1>Tum sachme merese baat nai karogi???(try again)</h1>;
                             case 3:   return <h1>Kabtak no dabaogi?</h1>;
                             case 4:   return <h1>Dabati raho,i have plenty of time</h1>;
                             case 5:   return <h1>Yaar?</h1>;
-                            case 16:    return <h1>No button hi hata dia! LOL!,  Abb to yes dabana padhega hihihihi</h1>
+                            case 16:    return <h1>No button hi hata dia! LOL!,  Abb to sorry deni padhegi hihihihihi</h1>
                     
                             default:      return <h1>Pretty Pleas{e}?</h1>
                           }
                     })()
-                }</div>:<h1>Love You Jyotuduuuuu!!{"(Happy Valentine's)"}</h1>}
+                }</div>:<h1>Thank You Ambikaaaaaa!! mujhe pata tha tum mujhe maaf kie bagair nai reh sakti :)</h1>}
             {yes?<div>
                 <button className="btn" style={yesstyle} onClick={()=>{setYes(false)}}></button>
                 {count<16?<button className="nobutton" style={nostyle} onClick={()=>{setCount(count+1); if(count>5)setE(e+"e")}}></button>:null}
